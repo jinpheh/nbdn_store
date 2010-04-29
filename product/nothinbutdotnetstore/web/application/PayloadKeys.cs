8a4c197092992efabb6fbcf4ed6b5cf5dfb96d68
Jin Pheh <jin.pheh@bitsinglass.com>
2010-04-29 11:34:02 -0600

    -added product payload keys

--- product/nothinbutdotnetstore/web/application/PayloadKeys.cs
+++ product/nothinbutdotnetstore/web/application/PayloadKeys.cs
@@ -11,5 +11,11 @@ namespace nothinbutdotnetstore.web.application
             public static readonly PayloadKey<DateTime> active_in_store_since = new PayloadKey<DateTime>("active_in_store_since");
             public static readonly PayloadKey<bool> is_active = new PayloadKey<bool>("is_active");
         }
+        public static class product
+        {
+            public static readonly PayloadKey<string> Name = new PayloadKey<string>("product_name");
+            public static readonly PayloadKey<string> Description = new PayloadKey<DateTime>("product_description");
+            public static readonly PayloadKey<decimal> Price = new PayloadKey<bool>("product_price");
+        }
     }
 }
